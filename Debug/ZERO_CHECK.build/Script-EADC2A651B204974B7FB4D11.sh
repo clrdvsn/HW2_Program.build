af9ac59b3b581f12f01461135e30c93061f10131
clrdvsn <clairedavison@Claires-iMac.local>
2016-10-09 13:16:47 -0600

    assignment 2

---
+++ Debug/ZERO_CHECK.build/Script-EADC2A651B204974B7FB4D11.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/clairedavison/Desktop/ISU\ HCI/ME\ 557/HCI-557-CG-master/assignments/02_3D_Modeling -f /Users/clairedavison/Desktop/ISU\ HCI/ME\ 557/HCI-557-CG-master/assignments/02_3D_Modeling/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
